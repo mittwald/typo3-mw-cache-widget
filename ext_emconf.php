33635b2607adcce943d503136ec3d1c48c76298a
Torben Hansen <derhansen@gmail.com>
2022-05-24 19:47:57 +0200

    [TASK] Updated version number to 1.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author_email' => 'support@mittwald.de',
   'author_company' => 'Mittwald CM Service GmbH',
   'state' => 'beta',
-  'version' => '1.1.0',
+  'version' => '1.1.1',
   'autoload' => 
   array (
     'psr-4' => 
